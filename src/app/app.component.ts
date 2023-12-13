eb6d6dbbd201cc73e9be0e38bd2db85e98c6b882
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-12 19:45:52 -0500

    emulators set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wordsmith';
+  constructor(private firestore: Firestore) {}
+
+  public ngOnInit(): void {
+    const testCollection = collection(this.firestore, 'test');
+    onSnapshot(testCollection, (snapshot) => {
+      snapshot.docs.forEach((element) => {
+        console.log('data: ', element.data());
+      });
+    });
+  }
 }
