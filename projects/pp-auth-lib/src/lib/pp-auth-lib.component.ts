5b7b9b8368863b4c40fe246e4e4e6b04f156b7af
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-21 11:00:29 -0500

    copy pasted login component code, added tailwind as a dependency

--- projects/pp-auth-lib/src/lib/pp-auth-lib.component.ts
+++ projects/pp-auth-lib/src/lib/pp-auth-lib.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Subscription, take, timer } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'lib-pp-auth-lib',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      pp-auth-lib works!
-    </p>
-  `,
-  styles: ``
+  imports: [CommonModule, MatTabsModule],
+  templateUrl: './pp-auth-lib.component.html',
+  styles: ``,
 })
 export class PpAuthLibComponent {
+  constructor(private router: Router) {}
+  public authMessageClearTimeout!: Subscription;
+  public remainingTime!: number;
+  public authMessage: { status: string; message: any } = {
+    status: '',
+    message: null,
+  };
+  /*
+  use the timer rx operator to clear the auth message after a few seconds
+   */
+  public clearAuthMessage() {
+    this.authMessageClearTimeout = timer(0, 1000)
+      .pipe(take(6))
+      .subscribe((val: number) => {
+        if (this.authMessage.status === 'success') {
+          this.remainingTime = 5 - val;
+          if (this.remainingTime === 0) {
+            this.authMessage = { status: '', message: null };
+            this.navigateMethod();
+          }
+        } else if (this.authMessage.status === 'error') {
+          this.remainingTime = 5 - val;
+          if (this.remainingTime === 0) {
+            this.authMessage = { status: '', message: null };
+          }
+        }
+      });
+  }
 
+  public navigateMethod() {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,43 +9,37 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { connectAuthEmulator } from '@angular/fire/auth';
 import { connectFirestoreEmulator } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(
-      provideFirebaseApp(() =>
-        initializeApp({
-          projectId: 'wordsmith-vocabulary-builder',
-          appId: '1:401318579949:web:2680d8f4947fdd803c06e4',
-          storageBucket: 'wordsmith-vocabulary-builder.appspot.com',
-          apiKey: 'AIzaSyAW6bxP66Cg8v7JmVGxll_95sbpaNYwSSg',
-          authDomain: 'wordsmith-vocabulary-builder.firebaseapp.com',
-          messagingSenderId: '401318579949',
-          measurementId: 'G-PX3C2ZLDCQ',
-        })
-      )
-    ),
-    importProvidersFrom(
-      provideAuth(() => {
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+        projectId: 'wordsmith-vocabulary-builder',
+        appId: '1:401318579949:web:2680d8f4947fdd803c06e4',
+        storageBucket: 'wordsmith-vocabulary-builder.appspot.com',
+        apiKey: 'AIzaSyAW6bxP66Cg8v7JmVGxll_95sbpaNYwSSg',
+        authDomain: 'wordsmith-vocabulary-builder.firebaseapp.com',
+        messagingSenderId: '401318579949',
+        measurementId: 'G-PX3C2ZLDCQ',
+    }))),
+    importProvidersFrom(provideAuth(() => {
         const auth = getAuth();
         if (!environment.production) {
-          connectAuthEmulator(auth, 'http://localhost:9099', {
-            disableWarnings: true,
-          });
+            connectAuthEmulator(auth, 'http://localhost:9099', {
+                disableWarnings: true,
+            });
         }
         return auth;
-      })
-    ),
-    importProvidersFrom(
-      provideFirestore(() => {
+    })),
+    importProvidersFrom(provideFirestore(() => {
         const firestore = getFirestore();
         if (!environment.production) {
-          connectFirestoreEmulator(firestore, 'localhost', 8080);
+            connectFirestoreEmulator(firestore, 'localhost', 8080);
         }
         return firestore;
-      })
-    ),
-  ],
+    })),
+    provideAnimations()
+],
 };
