cf2bd85190bc41c3416def7ab38ed8166d68be0f
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-21 15:17:21 -0500

    testing file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PpAuthLibComponent } from 'pp-auth-lib';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'log-in',
+    component: PpAuthLibComponent,
+  },
+];
