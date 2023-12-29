b6c2883b46fe0118288480bf20f9874d8643921d
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-29 12:01:21 -0500

    able to call openai endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,32 @@
 
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
+import { OpenAI } from 'openai';
+
+const openai = new OpenAI({
+  apiKey: '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',
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+export const helloWorld = onRequest(async (request, response) => {
+  const results = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are an English professor, here to judge if a sentence that your student constructed using a given word you just taught the student makes sense and uses the word correctly. When responding to your student, assume the tone and verbiage of master Yoda from the starwars.',
+      },
+      {
+        role: 'user',
+        content:
+          "the word is 'antibiotics'. the sentence is: 'Mary is sick, the doctor prescribed her with some antibiotics.'",
+      },
+    ],
+  });
+
   logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+  response.send(results.choices[0].message.content);
 });
