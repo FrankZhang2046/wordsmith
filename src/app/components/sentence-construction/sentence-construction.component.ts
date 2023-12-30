e6a8ee979ca7b1fa1c74b8fbb1a05868ec7b2b75
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-30 14:32:44 -0500

    created markups for form

--- src/app/components/sentence-construction/sentence-construction.component.ts
+++ src/app/components/sentence-construction/sentence-construction.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sentence-construction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './sentence-construction.component.html',
-  styleUrl: './sentence-construction.component.scss'
+  styleUrl: './sentence-construction.component.scss',
 })
-export class SentenceConstructionComponent {
-
-}
+export class SentenceConstructionComponent {}
