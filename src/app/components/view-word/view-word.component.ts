230bfd6cb0eafe1512a72282ea141be678b4bf1a
Frank Zhang <frankbusinessmail@gmail.com>
2023-12-30 11:27:06 -0500

    minor layout changes

--- src/app/components/view-word/view-word.component.ts
+++ src/app/components/view-word/view-word.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WordEntry } from '../../model/word-entry.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-view-word',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatDividerModule],
   templateUrl: './view-word.component.html',
   styleUrl: './view-word.component.scss',
 })
