ef18449f0eee4413817cfd4ceb3218ab4d0501d1
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-06 15:56:29 -0500

    updated auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,20 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '@angular/fire/auth';
 import { inject } from '@angular/core';
+import { WordService } from '../services/word.service';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const auth = inject(Auth);
+  const wordService = inject(WordService);
   const router = inject(Router);
   await auth.authStateReady();
   if (auth.currentUser) {
-    return true;
+    if (wordService.selectedWordSubject.value) {
+      return true;
+    } else {
+      console.log(`need to select a word`);
+      return router.createUrlTree(['/word-search']);
+    }
   } else {
     return router.createUrlTree(['/log-in']);
   }
