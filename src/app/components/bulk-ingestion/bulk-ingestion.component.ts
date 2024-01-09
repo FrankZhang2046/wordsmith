d151dc2d80fc183ed21ba4c1855dd9573d2a0fd4
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-09 15:34:53 -0500

    refined list parsing algorithm, able to map words out as mat-chips

--- src/app/components/bulk-ingestion/bulk-ingestion.component.ts
+++ src/app/components/bulk-ingestion/bulk-ingestion.component.ts
@@ -1,25 +1,44 @@
+import { MatChipsModule } from '@angular/material/chips';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-bulk-ingestion',
   standalone: true,
-  imports: [CommonModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatIconModule,
+  ],
   templateUrl: './bulk-ingestion.component.html',
   styleUrl: './bulk-ingestion.component.scss',
 })
 export class BulkIngestionComponent {
   public bulkWordString = new FormControl<string>('');
-  public printForm() {
-    console.log(this.returnListOfWords(this.bulkWordString.value || ''));
+  public listOfWords: string[] = [];
+  public printForm(event: Event) {
+    event.preventDefault();
+    this.listOfWords = this.returnListOfWords(this.bulkWordString.value || '');
   }
 
   public returnListOfWords(str: string): string[] {
-    return str
-      .split('\n')
-      .map((line) => line.replace(/[^a-zA-Z]/g, '').toLowerCase());
+    const wordList: string[] = [];
+    str.split('\n').forEach((line) => {
+      line
+        .replace(/[^a-zA-Z]/g, '')
+        .toLowerCase()
+        .split(' ')
+        .forEach((word) => {
+          wordList.push(word);
+        });
+    });
+    return wordList;
   }
 }
