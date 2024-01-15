4ebc03ae6946c05b23d85613a58bcbd11c67cf6c
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-15 12:07:12 -0500

    added pwa as a dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { APP_INITIALIZER } from '@angular/core';
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -17,6 +17,7 @@ import { environment } from '../environments/environment';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { PpAuthLibService } from 'pp-auth-lib';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export function initConfig(ppAuthService: PpAuthLibService) {
   return () => (ppAuthService.isProd = environment.production);
@@ -25,50 +26,47 @@ export function initConfig(ppAuthService: PpAuthLibService) {
 export const appConfig: ApplicationConfig = {
   providers: [
     {
-      provide: APP_INITIALIZER,
-      useFactory: initConfig,
-      deps: [PpAuthLibService],
-      multi: true,
+        provide: APP_INITIALIZER,
+        useFactory: initConfig,
+        deps: [PpAuthLibService],
+        multi: true,
     },
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideClientHydration(),
-    importProvidersFrom(
-      provideFirebaseApp(() =>
-        initializeApp({
-          projectId: 'wordsmith-vocabulary-builder',
-          appId: '1:401318579949:web:2680d8f4947fdd803c06e4',
-          storageBucket: 'wordsmith-vocabulary-builder.appspot.com',
-          apiKey: 'AIzaSyAW6bxP66Cg8v7JmVGxll_95sbpaNYwSSg',
-          authDomain: 'wordsmith-vocabulary-builder.firebaseapp.com',
-          messagingSenderId: '401318579949',
-          measurementId: 'G-PX3C2ZLDCQ',
-        })
-      ),
-      provideAuth(() => {
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+        projectId: 'wordsmith-vocabulary-builder',
+        appId: '1:401318579949:web:2680d8f4947fdd803c06e4',
+        storageBucket: 'wordsmith-vocabulary-builder.appspot.com',
+        apiKey: 'AIzaSyAW6bxP66Cg8v7JmVGxll_95sbpaNYwSSg',
+        authDomain: 'wordsmith-vocabulary-builder.firebaseapp.com',
+        messagingSenderId: '401318579949',
+        measurementId: 'G-PX3C2ZLDCQ',
+    })), provideAuth(() => {
         const auth = getAuth();
         if (!environment.production) {
-          connectAuthEmulator(auth, 'http://localhost:9099', {
-            disableWarnings: true,
-          });
+            connectAuthEmulator(auth, 'http://localhost:9099', {
+                disableWarnings: true,
+            });
         }
         return auth;
-      }),
-      provideFirestore(() => {
+    }), provideFirestore(() => {
         const firestore = getFirestore();
         if (!environment.production) {
-          connectFirestoreEmulator(firestore, 'localhost', 8080);
+            connectFirestoreEmulator(firestore, 'localhost', 8080);
         }
         return firestore;
-      }),
-      provideFunctions(() => {
+    }), provideFunctions(() => {
         const functions = getFunctions();
         if (!environment.production) {
-          connectFunctionsEmulator(functions, 'localhost', 5001);
+            connectFunctionsEmulator(functions, 'localhost', 5001);
         }
         return functions;
-      })
-    ),
+    })),
     provideAnimations(),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
