aa42f0afb32b9968c60a519686f12b896e735f9c
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-19 15:05:00 -0500

    UI improvements to feedback display component

--- src/app/components/feedback-display/feedback-display.component.ts
+++ src/app/components/feedback-display/feedback-display.component.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SentenceService } from '../../services/sentence.service';
 import { MatIconModule } from '@angular/material/icon';
 import { ResolveFbBgColorPipeTsPipe } from '../../pipes/resolve-fb-bg-color.pipe.ts.pipe';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-feedback-display',
   standalone: true,
-  imports: [CommonModule, MatIconModule, ResolveFbBgColorPipeTsPipe],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    ResolveFbBgColorPipeTsPipe,
+    MatButtonModule,
+  ],
   templateUrl: './feedback-display.component.html',
   styleUrl: './feedback-display.component.scss',
 })
