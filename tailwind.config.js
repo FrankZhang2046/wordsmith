19fc0112eb0435d3489d9ea8f6400ece980ee001
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-20 12:08:38 -0500

    updated tailwind config and some library code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
+    "./node_modules/pp-auth-lib/**/*.{html,ts,js,mjs}",
+    "./projects/pp-auth-lib/src/lib/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
