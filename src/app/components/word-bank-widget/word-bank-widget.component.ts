1eb9587494001be537fd4e31de44db1611085224
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-21 18:40:57 -0500

    able to view word bank entires in a list

--- src/app/components/word-bank-widget/word-bank-widget.component.ts
+++ src/app/components/word-bank-widget/word-bank-widget.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, effect, inject } from '@angular/core';
 import { WordStats } from '../../models/word-entry.model';
 import { WordService } from '../../services/word.service';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-word-bank-widget',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './word-bank-widget.component.html',
   styleUrl: './word-bank-widget.component.scss',
 })
