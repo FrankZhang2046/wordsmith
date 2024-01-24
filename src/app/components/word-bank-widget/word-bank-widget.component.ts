719f5cbb91b8b92d8c4defefe616dab1151b5700
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-23 20:23:49 -0500

    finishing word bank view

--- src/app/components/word-bank-widget/word-bank-widget.component.ts
+++ src/app/components/word-bank-widget/word-bank-widget.component.ts
@@ -2,21 +2,42 @@ import { Component, Input, effect, inject } from '@angular/core';
 import { WordStats } from '../../models/word-entry.model';
 import { WordService } from '../../services/word.service';
 import { MatListModule, MatSelectionListChange } from '@angular/material/list';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { debounceTime } from 'rxjs';
+import Fuse from 'fuse.js';
 
 @Component({
   selector: 'app-word-bank-widget',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './word-bank-widget.component.html',
   styleUrl: './word-bank-widget.component.scss',
 })
 export class WordBankWidgetComponent {
   private wordService = inject(WordService);
   public wordBankEntries: WordStats[] = [];
+  public filteredWordBankEntries: WordStats[] = [];
+  public wordBankSearchString: FormControl<string | null> = new FormControl('');
   constructor() {
     effect(() => {
       this.wordBankEntries = this.wordService.wordBankEntriesSignal();
     });
+
+    this.wordBankSearchString.valueChanges
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        if (value) {
+          const fuse = new Fuse(this.wordBankEntries, {
+            includeScore: true,
+            keys: ['word'],
+          });
+          this.filteredWordBankEntries = fuse
+            .search(value)
+            .map((result) => result.item);
+          console.log(`filtered results: `, this.filteredWordBankEntries);
+        }
+      });
   }
   public printWordEntry(event: MatSelectionListChange) {
     event.options.forEach((option) => console.log(option.value));
