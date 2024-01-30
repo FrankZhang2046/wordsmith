fbea056154f691c79f3595c0690264943b650e89
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-30 13:06:49 -0500

    set selectedWordSignal

--- src/app/components/word-detail/word-detail.component.ts
+++ src/app/components/word-detail/word-detail.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ViewWordComponent } from '../view-word/view-word.component';
+import { WordService } from '../../services/word.service';
 
 @Component({
   selector: 'app-word-detail',
   standalone: true,
-  imports: [],
   templateUrl: './word-detail.component.html',
   styleUrl: './word-detail.component.scss',
+  imports: [ViewWordComponent],
 })
 export class WordDetailComponent {
   public word: string = '';
+  private wordService = inject(WordService);
   constructor(private route: ActivatedRoute) {
     this.word = this.route.snapshot.paramMap.get('word') || '';
+    this.wordService.setSelectedWordByWord(this.word);
   }
 }
