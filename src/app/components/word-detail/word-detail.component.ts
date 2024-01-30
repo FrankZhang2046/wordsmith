e5c5fd81c065d1499ccc60f3ec6db593f6c108b4
Frank Zhang <frankbusinessmail@gmail.com>
2024-01-30 15:54:38 -0500

    map out sentence history entries

--- src/app/components/word-detail/word-detail.component.ts
+++ src/app/components/word-detail/word-detail.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, Pipe } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ViewWordComponent } from '../view-word/view-word.component';
 import { WordService } from '../../services/word.service';
 import { WordStats } from '../../models/word-entry.model';
 import { Timestamp } from '@angular/fire/firestore';
+import { UtilitiesService } from '../../services/utilities.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-word-detail',
   standalone: true,
   templateUrl: './word-detail.component.html',
   styleUrl: './word-detail.component.scss',
-  imports: [ViewWordComponent],
+  imports: [ViewWordComponent, MatButtonModule],
 })
 export class WordDetailComponent {
   public word: string = '';
+  private utilityService = inject(UtilitiesService);
   private wordService = inject(WordService);
   public selectedWordStatsSignal = this.wordService.selectedWordStatsSignal;
 
@@ -24,5 +27,9 @@ export class WordDetailComponent {
 
   public dateTimeFormatter(date: Timestamp): string {
     return date.toDate().toISOString().split('T')[0];
+  }
+
+  public backToDashboard(): void {
+    this.utilityService.navigateMethod('dashboard');
   }
 }
--- src/app/pipes/resolve-chip-bg-color.pipe.ts
+++ src/app/pipes/resolve-chip-bg-color.pipe.ts
@@ -1,21 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { WordImportStatus } from '../models/word-entry.model';
+import { WordImportStatus, WordStats } from '../models/word-entry.model';
+import { Timestamp } from '@angular/fire/firestore';
+
+interface SentenceHistoryEntry {
+  correct: boolean;
+  timestamp: Timestamp;
+  feedback: string;
+  sentence: string;
+}
 
 @Pipe({
   name: 'resolveChipBgColor',
   standalone: true,
 })
 export class ResolveChipBgColorPipe implements PipeTransform {
-  transform(word: WordImportStatus): string {
-    switch (word.imported) {
-      case 'done':
-        return '!bg-green-400';
-      case 'warn':
-        return '!bg-yellow-400';
-      case 'discard':
-        return '!bg-red-400';
-      default:
-        return '';
+  transform(word: WordImportStatus | SentenceHistoryEntry): string {
+    if (word.hasOwnProperty('imported')) {
+      switch ((word as WordImportStatus).imported) {
+        case 'done':
+          return '!bg-green-400';
+        case 'warn':
+          return '!bg-yellow-400';
+        case 'discard':
+          return '!bg-red-400';
+        default:
+          return '';
+      }
+    } else {
+      switch ((word as SentenceHistoryEntry).correct) {
+        case true:
+          return '!bg-green-400';
+        case false:
+          return '!bg-red-400';
+        default:
+          return '';
+      }
     }
   }
 }
